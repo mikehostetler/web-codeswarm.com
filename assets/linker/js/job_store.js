42406fe7b246f94dd678fb5de3e6659f83299424
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-10-28 16:34:16 +0000

    handling data update events

--- assets/linker/js/job_store.js
+++ assets/linker/js/job_store.js
@@ -8,10 +8,6 @@
   var PHASES = ['environment', 'prepare', 'test', 'deploy', 'cleanup'];
 
   var statusHandlers = {
-    'phase.done': function() {
-      this.phase = data.next;
-    },
-
     'started': function (time) {
       this.started = time;
       this.phase = 'environment';
@@ -57,6 +53,66 @@
       } else {
         console.error('Invalid "plugin data" method received from plugin', data.plugin, data.method, data)
       }
+    },
+
+    'phase.done': function (data) {
+      console.log('phase done', this);
+      if (! this.phases) return;
+      this.phases[data.phase].finished = data.time;
+      this.phases[data.phase].duration = data.elapsed
+      this.phases[data.phase].exitCode = data.code;
+      if (['prepare', 'environment', 'cleanup'].indexOf(data.phase) !== -1) {
+        this.phases[data.phase].collapsed = true;
+      }
+      if (data.phase === 'test') this.test_status = data.code;
+      if (data.phase === 'deploy') this.deploy_status = data.code;
+      if (!data.next || !this.phases[data.next]) return;
+      this.phase = data.next;
+      this.phases[data.next].started = data.time;
+    },
+    'command.comment': function (data) {
+      if (! this.phases) return;
+      var phase = this.phases[this.phase]
+        , command = _.extend({}, SKELS.command);
+      command.command = data.comment;
+      command.comment = true;
+      command.plugin = data.plugin;
+      command.finished = data.time;
+      phase.commands.push(command);
+    },
+    'command.start': function (data) {
+      if (! this.phases) return;
+      var phase = this.phases[this.phase]
+        , command = _.extend({}, SKELS.command, data);
+      command.started = data.time;
+      phase.commands.push(command);
+    },
+    'command.done': function (data) {
+      if (! this.phases) return;
+      var phase = this.phases[this.phase]
+        , command = phase.commands[phase.commands.length - 1];
+      command.finished = data.time;
+      command.duration = data.elapsed;
+      command.exitCode = data.exitCode;
+      command.merged = command._merged;
+    },
+    'stdout': function (text) {
+      if (! this.phases) return;
+      var command = ensureCommand(this.phases[this.phase]);
+      command.out += text;
+      command._merged += text;
+      this.std.out += text;
+      this.std.merged += text;
+      this.std.merged_latest = text;
+    },
+    'stderr': function (text) {
+      if (! this.phases) return;
+      var command = ensureCommand(this.phases[this.phase]);
+      command.err += text;
+      command._merged += text;
+      this.std.err += text;
+      this.std.merged += text;
+      this.std.merged_latest = text;
     }
   }
 
@@ -72,6 +128,7 @@
   function dashboard(cb) {
     var self = this;
     this.socket.emit('dashboard:jobs', function(jobs) {
+      console.log('jobs:', jobs);
       self.jobs.yours = jobs.yours;
       self.jobs.public = jobs.public;
       self.changed();
@@ -135,12 +192,12 @@
       old = jobs.splice(found, 1)[0];
       job.project.prev = old.project.prev;
     }
-    if (job.phases) {
-      // get rid of extra data - we don't need it.
-      // note: this won't be passed up anyway for public projects
-      cleanJob(job);
-    }
-    job.phase = 'environment';
+    // if (job.phases) {
+    //   // get rid of extra data - we don't need it.
+    //   // note: this won't be passed up anyway for public projects
+    //   cleanJob(job);
+    // }
+    //job.phase = 'environment';
     jobs.unshift(job);
     this.changed();
   };
@@ -162,13 +219,13 @@
     this.changeCallback();
   };
 
-
-  function cleanJob(job) {
-    delete job.phases;
-    delete job.std;
-    delete job.stdout;
-    delete job.stderr;
-    delete job.stdmerged;
-    delete job.plugin_data;
+  function ensureCommand(phase) {
+    var command = phase.commands[phase.commands.length - 1];
+    if (!command || typeof(command.finished) !== 'undefined') {
+      command = _.extend({}, SKELS.command);
+      phase.commands.push(command);
+    }
+    return command;
   }
+
 })(window);
