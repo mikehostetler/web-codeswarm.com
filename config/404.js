4c8656766c3f55623523e80c97c1f34c9dab035f
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-11-11 14:45:58 +0000

    hack: reacting to /:org/:repo" URLs

--- config/404.js
+++ config/404.js
@@ -2,12 +2,18 @@
  * Default 404 (Not Found) handler
  *
  * If no route matches are found for a request, Sails will respond using this handler.
- * 
+ *
  * This middleware can also be invoked manually from a controller or policy:
  * Usage: res.notFound()
  */
 
 module.exports[404] = function pageNotFound(req, res) {
+
+  /// HACK
+  /// to react to GET /:org/:repo
+  if (req.method == 'GET' && req.url.match(/^\/[^\/]+\/[^\/]+$/)) {
+    return res.render('layout.ejs');
+  }
 
   /*
    * NOTE: This function is Sails middleware-- that means that not only do `req` and `res`
