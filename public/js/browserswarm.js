a02dcb2cebb06601de1626c85aef24faaaf87736
Pedro Teixeira <pedro.teixeira@gmail.com>
2014-01-02 10:30:35 +0000

    localized font-awesome

--- public/js/browserswarm.js
+++ public/js/browserswarm.js
@@ -1293,7 +1293,7 @@ function configureApp($routeProvider, $locationProvider, $httpProvider) {
 }
 
 
-},{"./http_interceptor":33,"./strider":36}],11:[function(require,module,exports){
+},{"./http_interceptor":35,"./strider":38}],11:[function(require,module,exports){
 var App = require('../app');
 
 App.controller('AccountCtrl', ['$scope', '$sce', '$location', 'Strider', AccountCtrl]);
@@ -1923,7 +1923,7 @@ function ConfigCtrl($scope, $routeParams, $sce, $location, Strider) {
 
   };
 }
-},{"../app":10,"../lib/md5":35,"./config/_fix_template":14}],14:[function(require,module,exports){
+},{"../app":10,"../lib/md5":37,"./config/_fix_template":14}],14:[function(require,module,exports){
 module.exports = fixTemplate;
 
 function fixTemplate(s) {
@@ -2764,6 +2764,248 @@ function RootCrtl($scope, $rootScope, $location, Strider) {
   getUser();
 }
 },{"../app":10}],33:[function(require,module,exports){
+var app = require('../app');
+
+app.filter('ansi', ['$sce', function ($sce) {
+  return function (input) {
+    if (!input) return '';
+    var text = input.replace(/^[^\n\r]*\u001b\[2K/gm, '')
+                    .replace(/\u001b\[K[^\n\r]*/g, '')
+                    .replace(/[^\n]*\r([^\n])/g, '$1')
+                    .replace(/^[^\n]*\u001b\[0G/gm, '')
+                    .replace(/&/g, '&amp;')
+                    .replace(/</g, '&lt;')
+                    .replace(/>/g, '&gt;')
+                    .replace(/'/g, '&#39;')
+                    .replace(/"/g, '&quot;');
+    return $sce.trustAsHtml(ansifilter(text));
+  }
+}]);
+
+function ansiparse(str) {
+  //
+  // I'm terrible at writing parsers.
+  //
+  var matchingControl = null,
+      matchingData = null,
+      matchingText = '',
+      ansiState = [],
+      result = [],
+      output = "",
+      state = {},
+      eraseChar;
+
+  var handleResult = function(p) {
+    var classes = [];
+
+    p.foreground && classes.push(p.foreground);
+    p.background && classes.push('bg-' + p.background);
+    p.bold       && classes.push('bold');
+    p.italic     && classes.push('italic');
+    if (!p.text) {
+      return;
+    }
+    if (classes.length === 0) {
+      return output += p.text
+    }
+    var span = '<span class="' + classes.join(' ') + '">' + p.text + '</span>'
+    output += span
+  }
+  //
+  // General workflow for this thing is:
+  // \033\[33mText
+  // |     |  |
+  // |     |  matchingText
+  // |     matchingData
+  // matchingControl
+  //
+  // In further steps we hope it's all going to be fine. It usually is.
+  //
+
+  //
+  // Erases a char from the output
+  //
+  eraseChar = function () {
+    var index, text;
+    if (matchingText.length) {
+      matchingText = matchingText.substr(0, matchingText.length - 1);
+    }
+    else if (result.length) {
+      index = result.length - 1;
+      text = result[index].text;
+      if (text.length === 1) {
+        //
+        // A result bit was fully deleted, pop it out to simplify the final output
+        //
+        result.pop();
+      }
+      else {
+        result[index].text = text.substr(0, text.length - 1);
+      }
+    }
+  };
+
+  for (var i = 0; i < str.length; i++) {
+    if (matchingControl !== null) {
+      if (matchingControl == '\033' && str[i] == '\[') {
+        //
+        // We've matched full control code. Lets start matching formating data.
+        //
+
+        //
+        // "emit" matched text with correct state
+        //
+        if (matchingText) {
+          state.text = matchingText;
+          handleResult(state);
+          state = {};
+          matchingText = "";
+        }
+
+        matchingControl = null;
+        matchingData = '';
+      }
+      else {
+        //
+        // We failed to match anything - most likely a bad control code. We
+        // go back to matching regular strings.
+        //
+        matchingText += matchingControl + str[i];
+        matchingControl = null;
+      }
+      continue;
+    }
+    else if (matchingData !== null) {
+      if (str[i] == ';') {
+        //
+        // `;` separates many formatting codes, for example: `\033[33;43m`
+        // means that both `33` and `43` should be applied.
+        //
+        // TODO: this can be simplified by modifying state here.
+        //
+        ansiState.push(matchingData);
+        matchingData = '';
+      }
+      else if (str[i] == 'm') {
+        //
+        // `m` finished whole formatting code. We can proceed to matching
+        // formatted text.
+        //
+        ansiState.push(matchingData);
+        matchingData = null;
+        matchingText = '';
+
+        //
+        // Convert matched formatting data into user-friendly state object.
+        //
+        // TODO: DRY.
+        //
+        ansiState.forEach(function (ansiCode) {
+          if (ansiparse.foregroundColors[ansiCode]) {
+            state.foreground = ansiparse.foregroundColors[ansiCode];
+          }
+          else if (ansiparse.backgroundColors[ansiCode]) {
+            state.background = ansiparse.backgroundColors[ansiCode];
+          }
+          else if (ansiCode == 39) {
+            delete state.foreground;
+          }
+          else if (ansiCode == 49) {
+            delete state.background;
+          }
+          else if (ansiparse.styles[ansiCode]) {
+            state[ansiparse.styles[ansiCode]] = true;
+          }
+          else if (ansiCode == 22) {
+            state.bold = false;
+          }
+          else if (ansiCode == 23) {
+            state.italic = false;
+          }
+          else if (ansiCode == 24) {
+            state.underline = false;
+          }
+        });
+        ansiState = [];
+      }
+      else {
+        matchingData += str[i];
+      }
+      continue;
+    }
+
+    if (str[i] == '\033') {
+      matchingControl = str[i];
+    }
+    else if (str[i] == '\u0008') {
+      eraseChar();
+    }
+    else {
+      matchingText += str[i];
+    }
+  }
+
+  if (matchingText) {
+    state.text = matchingText + (matchingControl ? matchingControl : '');
+    handleResult(state);
+  }
+  return output;
+}
+
+ansiparse.foregroundColors = {
+  '30': 'black',
+  '31': 'red',
+  '32': 'green',
+  '33': 'yellow',
+  '34': 'blue',
+  '35': 'magenta',
+  '36': 'cyan',
+  '37': 'white',
+  '90': 'grey'
+};
+
+ansiparse.backgroundColors = {
+  '40': 'black',
+  '41': 'red',
+  '42': 'green',
+  '43': 'yellow',
+  '44': 'blue',
+  '45': 'magenta',
+  '46': 'cyan',
+  '47': 'white'
+};
+
+ansiparse.styles = {
+  '1': 'bold',
+  '3': 'italic',
+  '4': 'underline'
+};
+
+function ansifilter(data, plaintext, cache) {
+
+  // handle the characters for "delete line" and "move to start of line"
+  var startswithcr = /^[^\n]*\r[^\n]/.test(data);
+  var output = ansiparse(data);
+
+  var res = output.replace(/\033/g, '');
+  if (startswithcr) res = '\r' + res;
+
+  return res;
+}
+
+
+},{"../app":10}],34:[function(require,module,exports){
+var App = require('../app');
+
+App.filter('percentage', function () {
+  return function (input, prec) {
+    if (!input && parseInt(input) !== 0) return '';
+    var by = Math.pow(10, prec || 1)
+    return parseInt(parseFloat(input) * by, 10)/by + '%'
+  }
+});
+
+},{"../app":10}],35:[function(require,module,exports){
 module.exports = ['$rootScope', '$q', function($scope, $q) {
 
   function success(response) {
@@ -2792,7 +3034,7 @@ module.exports = ['$rootScope', '$q', function($scope, $q) {
   }
 
 }];
-},{}],34:[function(require,module,exports){
+},{}],36:[function(require,module,exports){
 var EventEmitter = require('events').EventEmitter;
 var inherits     = require('util').inherits;
 var extend       = require('xtend');
@@ -3119,7 +3361,7 @@ var SKELS = {
     commands: []
   }
 }
-},{"events":2,"util":3,"xtend":5}],35:[function(require,module,exports){
+},{"events":2,"util":3,"xtend":5}],37:[function(require,module,exports){
 function md5cycle(x, k) {
 var a = x[0], b = x[1], c = x[2], d = x[3];
 
@@ -3307,7 +3549,7 @@ return (msw << 16) | (lsw & 0xFFFF);
 
 
 module.exports = md5;
-},{}],36:[function(require,module,exports){
+},{}],38:[function(require,module,exports){
 var JobStore = require('./job_store');
 var jobStore = JobStore();
 
@@ -3436,5 +3678,5 @@ S.request = function(method, url, body, cb) {
 
   return req;
 }
-},{"./job_store":34}]},{},[10,11,12,13,24,25,26,27,28,29,30,31,32,14,15,16,17,18,19,20,21,22,23])
+},{"./job_store":36}]},{},[10,11,12,13,24,25,26,27,28,29,30,31,32,14,15,16,17,18,19,20,21,22,23,33,34])
 ;
