a3e2b07ad88fad08d44a9a33ac2af994e84f2efc
Pedro Teixeira <i@pgte.me>
2014-01-02 10:59:07 +0000

    supporting variable configuration for strider hostname and port

---
+++ config/index.js
@@ -0,0 +1,5 @@
+var config = require('konphyg')(__dirname);
+
+module.exports = {
+  strider: config('strider')
+};
--- proxy.js
+++ proxy.js
@@ -1,14 +1,10 @@
 var httpProxy = require('http-proxy');
+var config    = require('./config');
 
 var proxy = new httpProxy.RoutingProxy();
 
 module.exports = proxyRoute;
 
-var proxyConf = {
-  host: 'localhost',
-  port: 3000
-};
-
 function proxyRoute(req, res) {
-  proxy.proxyRequest(req, res, proxyConf);
+  proxy.proxyRequest(req, res, config.strider);
 }
